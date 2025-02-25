b8c73fd01438a29c1b99b5e3d91aab121bf5d9e0
amitBAGDE <52626229+amitBAGDE@users.noreply.github.com>
2025-02-25 23:43:18 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,14 +2,10 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
 import psycopg2
 
-# Replace with your actual database URL
-DATABASE_URL = "postgresql://postgres:Pass%40123@localhost/postgres"  # Updated with default credentials
-
+DATABASE_URL = "postgresql://postgres:Pass%40123@localhost/postgres" 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
-
 def get_db():
     db = SessionLocal()
     try:
