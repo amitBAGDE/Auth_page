6366f499217016d435c85dca474588fccf2ed783
amitBAGDE <52626229+amitBAGDE@users.noreply.github.com>
2025-02-25 23:45:23 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship
-from database import Base  # Import Base from database.py
+from database import Base
 from datetime import datetime
-
 
 class User(Base):
     __tablename__ = "users"
@@ -13,10 +12,9 @@ class User(Base):
     mobile = Column(String, unique=True, index=True)
     address = Column(String)
     two_step_verification = Column(Boolean, default=False)
-    created_by = Column(Integer, ForeignKey("users.id"))  # Self-referential ForeignKey
+    created_by = Column(Integer, ForeignKey("users.id")) 
     created_at = Column(DateTime)
-    password = Column(String)  # Store hashed passwords
-    otp = Column(String, nullable=True)  # Store OTP for 2-step verification
-    otp_expiry = Column(DateTime, nullable=True)  # OTP expiry time
+    password = Column(String)  
+    otp = Column(String, nullable=True) 
+    otp_expiry = Column(DateTime, nullable=True) 
 
-    # created_by_user = relationship("User", remote_side=[id]) # Removed to prevent error
